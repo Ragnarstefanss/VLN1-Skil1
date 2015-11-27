7ba38544322ab24f0522ac7e17ed4c4556d38def
Ragnarstefanss <ragnars15@ru.is>
2015-11-27 10:14:54 +0000

    added choices

--- main.cpp
+++ main.cpp
@@ -5,12 +5,17 @@ using namespace std;
 // Þórdís að prufa git.
 int main()
 {
-    int tala;
+    int input_val;
 
-    cout << "Skrifadu tolu: ";
-    cin >> tala;
+    cout << "1) Add a new person" << endl
+         << "2) Show list of persons" << endl
+         << "3) Search for a person" << endl;
 
-    cout << "Talan er: " << tala << endl;
+    cout << "Pick a number: ";
+    cin >> input_val;
+    cout << endl;
+
+    cout << "You picked the number: " << input_val << endl;
 
     return 0;
 }
