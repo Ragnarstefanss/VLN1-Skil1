8b82da702a29898f31557519771e1054e7cdf4cc
Maciuska <Maciuska14@gmail.com>
2015-11-29 13:08:36 +0000

    Create personal.cpp

---
+++ personal.cpp
@@ -0,0 +1,42 @@
+#include "personal.h"
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+
+ifstream names("names.txt");
+ifstream genders("genders.txt");
+ifstream births("births.txt");
+ifstream deaths("deaths.txt");
+
+using namespace std;
+
+Personal::Personal()
+{
+
+}
+
+void Personal::loadPersonal()
+{
+
+}
+
+void loadNames()
+{
+
+}
+
+void loadGenders()
+{
+
+}
+
+void loadBirths()
+{
+
+}
+
+void loadDeaths()
+{
+
+}
